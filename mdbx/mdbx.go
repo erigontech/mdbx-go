459030061a0c8b22e1d2475d6f227e2de2cc93c1
alex.sharov <AskAlexSharov@gmail.com>
2021-05-11 10:30:42 +0700

    alex

--- mdbx/mdbx.go
+++ mdbx/mdbx.go
@@ -135,7 +135,7 @@ package mdbx
 #cgo darwin CFLAGS: -O2 -g -Werror -Wno-deprecated-declarations -Wextra -Wpedantic -fPIC -fvisibility=hidden -pthread -Wno-error=attributes -Wno-implicit-fallthrough -Wno-unused-function -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers -DNDEBUG=1
 #cgo windows CFLAGS: -O2 -g -Werror -Wno-deprecated-declarations -Wextra -Wpedantic -fPIC -fvisibility=hidden -pthread -Wno-error=attributes -Wno-implicit-fallthrough -Wno-unused-function -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers -DNDEBUG=1 -Wno-cast-function-type
 
-#cgo LDFLAGS: -v -L. -L"${SRCDIR}"/../libmdbx/build -lmdbx
+#cgo LDFLAGS: -v -L. -L"${SRCDIR}"/../libmdbx/build -lmdbx-static
 
 //#cgo windows LDFLAGS: -l${SRCDIR}/dist/Debug/mdbx-static.lib -l${SRCDIR}/ntdll.dll
 
