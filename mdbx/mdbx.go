8775a15ec877ee5a3d97b05abff47d6d790a3e27
alex.sharov <AskAlexSharov@gmail.com>
2021-05-14 09:09:22 +0700

    cp config.h

--- mdbx/mdbx.go
+++ mdbx/mdbx.go
@@ -136,7 +136,7 @@ package mdbx
 //#cgo windows CFLAGS: -O2 -g -Werror -Wno-deprecated-declarations -Wextra -Wpedantic -fPIC -fvisibility=hidden -pthread -Wno-error=attributes -Wno-implicit-fallthrough -Wno-unused-function -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers -DNDEBUG=1 -Wno-cast-function-type
 
 #cgo CFLAGS: -pthread -W -Wall -Wno-format -Wno-implicit-fallthrough -Wno-cast-function-type -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers -O2 -g -Wno-deprecated-declarations
-//#cgo windows LDFLAGS: -v -LC:/WINDOWS/SYSTEM32/ -lntdll
+#cgo windows LDFLAGS: -v -LC:/WINDOWS/SYSTEM32/ -lntdll
 
 //#cgo LDFLAGS: -v -L. -L"${SRCDIR}"/../libmdbx/build -lmdbx
 
