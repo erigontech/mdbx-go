88ebb22288bc90e8bcd2ea5d8edff80710eddf5a
alex.sharov <AskAlexSharov@gmail.com>
2022-11-14 07:22:44 +0300

    save

--- mdbx/txn.go
+++ mdbx/txn.go
@@ -228,9 +228,13 @@ type CommitLatencyGC struct {
 	WorkCounter uint32
 	SelfCounter uint32
 
-	PnlMergeTime   time.Duration
-	PnlMergeVolume uint64
-	PnlMergeCalls  uint32
+	WorkPnlMergeTime   time.Duration
+	WorkPnlMergeVolume uint64
+	WorkPnlMergeCalls  uint32
+
+	SelfPnlMergeTime   time.Duration
+	SelfPnlMergeVolume uint64
+	SelfPnlMergeCalls  uint32
 
 	/** \brief Время затраченное на чтение и поиск внтури GC
 	 *  для целей поддержки и обновления самой GC. */
@@ -266,26 +270,29 @@ func (txn *Txn) commit() (CommitLatency, error) {
 		Ending:      toDuration(_stat.ending),
 		Whole:       toDuration(_stat.whole),
 		GCDetails: CommitLatencyGC{
-			WorkRtime:      toDuration(_stat.gc_prof.work_rtime_monotonic),
-			WorkRtimeCPU:   toDuration(_stat.gc_prof.work_rtime_cpu),
-			WorkRsteps:     uint32(_stat.gc_prof.work_rsteps),
-			WorkRxpages:    uint32(_stat.gc_prof.work_xpages),
-			WorkMajflt:     uint32(_stat.gc_prof.work_majflt),
-			PnlMergeTime:   toDuration(_stat.gc_prof.pnl_merge.time),
-			PnlMergeVolume: uint64(_stat.gc_prof.pnl_merge.volume),
-			PnlMergeCalls:  uint32(_stat.gc_prof.pnl_merge.calls),
-			SelfRtime:      toDuration(_stat.gc_prof.self_rtime_monotonic),
-			SelfRtimeCPU:   toDuration(_stat.gc_prof.self_rtime_cpu),
-			SelfXtime:      toDuration(_stat.gc_prof.self_xtime_monotonic),
-			SelfRsteps:     uint32(_stat.gc_prof.self_rsteps),
-			SelfXpages:     uint32(_stat.gc_prof.self_xpages),
-			Wloops:         uint32(_stat.gc_prof.wloops),
-			Coalescences:   uint32(_stat.gc_prof.coalescences),
-			Wipes:          uint32(_stat.gc_prof.wipes),
-			Flushes:        uint32(_stat.gc_prof.flushes),
-			Kicks:          uint32(_stat.gc_prof.kicks),
-			SelfCounter:    uint32(_stat.gc_prof.self_counter),
-			WorkCounter:    uint32(_stat.gc_prof.work_counter),
+			WorkRtime:          toDuration(_stat.gc_prof.work_rtime_monotonic),
+			WorkRtimeCPU:       toDuration(_stat.gc_prof.work_rtime_cpu),
+			WorkRsteps:         uint32(_stat.gc_prof.work_rsteps),
+			WorkRxpages:        uint32(_stat.gc_prof.work_xpages),
+			WorkMajflt:         uint32(_stat.gc_prof.work_majflt),
+			WorkPnlMergeTime:   toDuration(_stat.gc_prof.pnl_merge_work.time),
+			WorkPnlMergeVolume: uint64(_stat.gc_prof.pnl_merge_work.volume),
+			WorkPnlMergeCalls:  uint32(_stat.gc_prof.pnl_merge_work.calls),
+			SelfPnlMergeTime:   toDuration(_stat.gc_prof.pnl_merge_self.time),
+			SelfPnlMergeVolume: uint64(_stat.gc_prof.pnl_merge_self.volume),
+			SelfPnlMergeCalls:  uint32(_stat.gc_prof.pnl_merge_self.calls),
+			SelfRtime:          toDuration(_stat.gc_prof.self_rtime_monotonic),
+			SelfRtimeCPU:       toDuration(_stat.gc_prof.self_rtime_cpu),
+			SelfXtime:          toDuration(_stat.gc_prof.self_xtime_monotonic),
+			SelfRsteps:         uint32(_stat.gc_prof.self_rsteps),
+			SelfXpages:         uint32(_stat.gc_prof.self_xpages),
+			Wloops:             uint32(_stat.gc_prof.wloops),
+			Coalescences:       uint32(_stat.gc_prof.coalescences),
+			Wipes:              uint32(_stat.gc_prof.wipes),
+			Flushes:            uint32(_stat.gc_prof.flushes),
+			Kicks:              uint32(_stat.gc_prof.kicks),
+			SelfCounter:        uint32(_stat.gc_prof.self_counter),
+			WorkCounter:        uint32(_stat.gc_prof.work_counter),
 		},
 	}
 	if ret != success {
