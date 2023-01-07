4c2496d26631510a1ebbb8ea94022fbc76406923
alex.sharov <AskAlexSharov@gmail.com>
2023-01-07 11:21:03 +0700

    save

--- mdbx/env_windows.go
+++ mdbx/env_windows.go
@@ -6,7 +6,7 @@ package mdbx
 import "C"
 
 func (env *Env) Path() (string, error) {
-	var cpath C.wchar_t
+	var cpath *C.wchar_t
 	ret := C.mdbx_env_get_pathW(env._env, &cpath)
 	if ret != success {
 		return "", operrno("mdbx_env_get_path", ret)
