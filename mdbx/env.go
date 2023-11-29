f1920077b0cd6700799153c9962123a0b9b1feb0
alex.sharov <AskAlexSharov@gmail.com>
2023-11-29 12:14:30 +0700

    save

--- mdbx/env.go
+++ mdbx/env.go
@@ -104,6 +104,7 @@ const (
 	OptSpillMinDenominator          = C.MDBX_opt_spill_min_denominator
 	OptSpillParent4ChildDenominator = C.MDBX_opt_spill_parent4child_denominator
 	OptMergeThreshold16dot16Percent = C.MDBX_opt_merge_threshold_16dot16_percent
+	OptGCTimeLimit                  = C.MDBX_opt_gc_time_limit
 )
 
 var (
