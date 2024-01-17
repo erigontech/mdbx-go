10cefd48b92e84c2a654ce389817a7fbef074cfe
alex.sharov <AskAlexSharov@gmail.com>
2024-01-17 14:20:49 +0700

    save

---
+++ mdbxdist/config.h
@@ -0,0 +1,4 @@
+#define MDBX_BUILD_TIMESTAMP "unknown"
+#define MDBX_BUILD_FLAGS "-DNDEBUG=1 -std=gnu++2b -Wno-unknown-warning-option -Wno-enum-int-mismatch -Wno-strict-prototypes -Wno-unused-but-set-variable -lm"
+#define MDBX_BUILD_COMPILER "Apple clang version 15.0.0 (clang-1500.0.40.1)"
+#define MDBX_BUILD_TARGET "x86_64-apple-darwin23.0.0"
