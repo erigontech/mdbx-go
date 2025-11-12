70321464ab9c0d7f67cc6ad1a54da6947146e7ea
JkLondon <me@ilyamikheev.com>
2025-11-13 00:39:36 +0100

    fix linter

--- mdbx/env_not_win.go
+++ mdbx/env_not_win.go
@@ -7,15 +7,14 @@ package mdbx
 */
 import "C"
 
-//TODO: fix me please
-
 // Path returns the path argument passed to Open.  Path returns a non-nil error
 // if env.Open() was not previously called.
 //
 // See mdbx_env_get_path.
+//
+//nolint:gocritic // reason: false positive on dupSubExpr
 func (env *Env) Path() (string, error) {
 	var cpath *C.char
-	//nolint:dupSubExpr // false positive from Cgo (C.mdbx_env_get_path)
 	ret := C.mdbx_env_get_path(env._env, &cpath)
 	if ret != success {
 		return "", operrno("mdbx_env_get_path", ret)
